4e2f3005ef033fea0d7641d05098d9a33f243b49
Mary Camacho <maryfcamacho@gmail.com>
2016-04-15 11:29:58 -0600

    updated notes on algorithms

--- algorithms/problem1-solved.js
+++ algorithms/problem1-solved.js
@@ -32,7 +32,7 @@ function maximum(arr) {
 console.log(maximum([3,4,5]) == 5);
 console.log(maximum([-10, 2]) == 2);
 
-//Write a function which takes an array of numbers and returns the average of that array.
+//Write a function which takes an array of numbers and returns the average of that array. Using the reduce method of an array - which I'm using to reduce the elements in the array from left to right by combining the first two, then combining the first two until I have only 1 element - which if they are all integers is essentially the sum of the integers.
 
 function averageArray (list) {
 	var sumArray = list.reduce(function(pv, cv) {
@@ -45,7 +45,7 @@ console.log(averageArray([2, 4, 6]) == 4);
 console.log(averageArray([0, 0, 0, 10]) == 2.5);
 
 //Write a function which takes an array of integers > 0 and returns  the first integer which does not appear in that array.
-// I'm assuming that the lists provided are sorted.
+// I'm assuming that the lists provided are sorted. Using the method indexOf to do a test for an element not being present. Because using 'i' was confusing to me when trying to apply the logic of 'indexOf' I changed it to call it possibleMissing. So as I loop through the elements in the array, I am essentially saying for each possibleMissing value test to is it missing. Because I'm only looking for the first one - this works perfectly as it tests left to right and I return it.
 
 function missingFirst (list) {
 	for (var possibleMissing = 1; possibleMissing <= maximum(list)+1; possibleMissing++) {
@@ -54,6 +54,9 @@ function missingFirst (list) {
 		}
 	}
 }
+
+console.log(missingFirst([1,2,5]));
+console.log( missingFirst([1,2,3,4,5]));
 
 console.log(missingFirst([1,2,5]) == 3);
 console.log(missingFirst([1,2,3,4,5]) == 6);
@@ -69,7 +72,7 @@ function dupsRemoved (list) {
 console.log(dupsRemoved([1,1,2,3,1,2,3]).toString() == [1,2,3].toString());
 console.log(dupsRemoved([1,4,4,4,2,2,4,4,4]).toString() == [1,4,2].toString());
 
-// Here is the for loop version that will work everywhere.
+// Here is the for loop version that will work everywhere but using again the indexOf method which like the function above it searches for the first index where a particular element is found. When indexOf is not true (because internally it is running a strict  truth test) it returns -1. So I'm using it to test if it is not negative and if that is true I'm pushing it into my clean list.
 
 function dupsRemoved2 (list) {
 	var cleanList = [];
